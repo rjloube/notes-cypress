400214ddd6f9b53b9880319be83f50b38a0e4f21
russell-loube-optimizely <russell.loube@optimizely.com>
2024-09-05 13:21:45 -0500

    reset db for testing, added test for changing note importance

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe("Note app", function () {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      name: "Root User",
+      username: "root",
+      password: "sekret",
+    };
+    cy.request("POST", "http://localhost:3001/api/users", user);
     cy.visit("http://localhost:5173");
   });
 
@@ -32,6 +39,21 @@ describe("Note app", function () {
       cy.get("#note-input").type("a note created by cypress");
       cy.contains("save").click();
       cy.contains("a note created by cypress");
+    });
+
+    describe("and a note exists", function () {
+      beforeEach(function () {
+        cy.contains("new note").click();
+        cy.get("#note-input").type("another note cypress");
+        cy.contains("save").click();
+      });
+
+      it("it can be made not important", function () {
+        cy.contains("another note cypress")
+          .contains("make not important")
+          .click();
+        cy.contains("another note cypress").contains("make important");
+      });
     });
   });
 });
